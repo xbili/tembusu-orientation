a6ce3e031d45f8b755bbac3d71bfe7744280192e
Bili Xu <xubili93@gmail.com>
2015-07-05 21:14:32 +0800

    Update router configurations to point to correct templates

--- src/app/app.js
+++ src/app/app.js
@@ -7,32 +7,33 @@ angular.module( 'ngVn', [
   'ui.router'
 ])
 
-.config( function myAppConfig ( $stateProvider, $urlRouterProvider) {
-  // $urlRouterProvider.otherwise('/home');
+.config( function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
 
   $stateProvider
     .state('home',
         {
             url: '/home',
-            templateUrl: '../src/app/home/home.tpl.html'
+            controller: 'HomeCtrl',
+            templateUrl: 'home/home.tpl.html'
         })
     .state('updates',
         {
             url: '/updates',
-            templateUrl: '../src/app/updates/updates.tpl.html'
+            controller: 'UpdatesCtrl',
+            templateUrl: 'updates/updates.tpl.html'
         })
     .state('about',
         {
             url: '/about',
-            templateUrl: '../src/app/about/about.tpl.html'
+            controller: 'AboutCtrl',
+            templateUrl: 'about/about.tpl.html'
         });
 })
 
-.run( function run () {
-})
-
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
-  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+  $scope.$on('$stateChangeSuccess',
+             function(event, toState, toParams, fromState, fromParams) {
     if ( angular.isDefined( toState.data.pageTitle ) ) {
       $scope.pageTitle = toState.data.pageTitle + ' | ngVn' ;
     }
