0e10282b1e7880b6ac268acbaa7bf2d5c87640a5
Bili Xu <xubili93@gmail.com>
2015-07-10 01:23:49 +0800

    Placeholders for about page layout, set up controller and page link

--- src/app/about/about.js
+++ src/app/about/about.js
@@ -4,17 +4,8 @@ angular.module( 'ngVn.about', [
   'ui.bootstrap'
 ])
 
-.config(function config( $stateProvider ) { 
-    // Only define sub-routes here
-})
-
 .controller( 'AboutCtrl', function AboutCtrl( $scope ) {
-  // This is simple a demo for UI Boostrap.
-  $scope.dropdownDemoItems = [
-    "The first choice!",
-    "And another choice for you.",
-    "but wait! A third!"
-  ];
+    $scope.teamMembers = ['amit', 'bili'];
 })
 
 ;
