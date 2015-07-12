ccd278f9e060a854d5524483550e6d0947c44310
Bili Xu <xubili93@gmail.com>
2015-07-12 18:15:38 +0800

    About page implementation with data

--- src/app/about/about.js
+++ src/app/about/about.js
@@ -5,8 +5,36 @@ angular.module( 'ngVn.about', [
 ])
 
 .controller( 'AboutCtrl', function AboutCtrl( $scope ) {
-    $scope.teamMembers = ['amit', 'bili'];
+    $scope.teamMembers = teamMembers;
 })
 
 ;
+
+
+var teamMembers = [
+    {
+        name:'Moazzam Khan',
+        shortname: 'moazzam',
+        title: 'President CSC',
+        about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name:'Mocha',
+        shortname: 'moazzam',
+        title: 'Resistance',
+        about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name:'Redis',
+        shortname: 'moazzam',
+        title: 'Farmer',
+        about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name:'Mongo',
+        shortname: 'moazzam',
+        title: 'President CSC',
+        about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    }
+];
 
