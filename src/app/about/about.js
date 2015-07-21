c089432ed9c4ce6e8d68d48f88cf79b0878fd7f6
Bili Xu <xubili93@gmail.com>
2015-07-22 02:16:39 +0800

    Revamp about page and load in actual data

--- src/app/about/about.js
+++ src/app/about/about.js
@@ -4,37 +4,14 @@ angular.module( 'ngVn.about', [
   'ui.bootstrap'
 ])
 
-.controller( 'AboutCtrl', function AboutCtrl( $scope ) {
-    $scope.teamMembers = teamMembers;
+.controller( 'AboutCtrl', function AboutCtrl( $scope, $http ) {
+    $http.get('assets/data/team/team.json')
+        .then(function(res) {
+            $scope.mainTeam = res.data[0];
+            $scope.subComm = res.data.slice(1, -1);
+            $scope.finComm = res.data.slice(-1)[0];
+        });
 })
 
 ;
-
-
-var teamMembers = [
-    {
-        name:'Moazzam Khan',
-        shortname: 'moazzam',
-        title: 'President CSC',
-        about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    },
-    {
-        name:'Mocha',
-        shortname: 'moazzam',
-        title: 'Resistance',
-        about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    },
-    {
-        name:'Redis',
-        shortname: 'moazzam',
-        title: 'Farmer',
-        about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    },
-    {
-        name:'Mongo',
-        shortname: 'moazzam',
-        title: 'President CSC',
-        about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    }
-];
 
